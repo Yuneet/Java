1cfb71d0e6b85354a2630af610fdf4ed30b960ef
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-03-12 11:14:58 +0900

    fdafdsaf

--- SampleProject/src/Hello.java
+++ SampleProject/src/Hello.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Hello {
 
 	public static void main(String[] args) {
-		for(int i=0;i==10;i++)
-		System.out.println("201787041 김규빈");
+			char ch = 0312;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("입력해라");
+			String text = sc.next();
+			set(text.toString());
 	}
-
+	
+	public static void set(String string) {
+		if(string == "준수는"){
+			System.out.println("바보다");			
+		}else{
+			System.out.println("test");
+		}
+	}
 }
