e0195e2bef4372affaf7ef3bbbaa501efbd9b3c2
Yuneet <76992544+Yuneet@users.noreply.github.com>
2021-05-14 10:59:19 +0900

    fdsaf
    fdsafd

---
+++ staticClass/src/staticClass/Abc.java
@@ -0,0 +1,15 @@
+package staticClass;
+
+public class Abc {
+	int i;
+	
+	public static int display() {
+		System.out.println("201787041");
+		System.out.println("±è±Ôºó");
+		return 6;
+	}
+	
+	public static void main(String[] args) {
+		display();
+	}
+}
